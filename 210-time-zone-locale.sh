4a161993eba03c4e97f1a1c36b5f2b01f4dc9d7c
Dejan Kutle <kronikpillow@gmail.com>
2022-06-23 09:36:26 +0200

    changed default timezone settings

--- 210-time-zone-locale.sh
+++ 210-time-zone-locale.sh
@@ -3,10 +3,10 @@
 echo "configuring time zone and locale"
 ln -sf /usr/share/zoneinfo/Europe/Belgrade /etc/localtime
 hwclock --systohc
-sed -i '161 s/# *//' /etc/locale.gen
-sed -i '178 s/# *//' /etc/locale.gen
+sed -i '160 s/# *//' /etc/locale.gen
+sed -i '177 s/# *//' /etc/locale.gen
+sed -i '436 s/# *//' /etc/locale.gen
 sed -i '437 s/# *//' /etc/locale.gen
-sed -i '438 s/# *//' /etc/locale.gen
 locale-gen
 touch /etc/locale.conf
 grep -q "LANG=en_US.UTF-8" /etc/locale.conf || echo "LANG=en_US.UTF-8" >> /etc/locale.conf
