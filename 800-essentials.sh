495120f99b84b7bf8dfd415e8897d81dfa235176
Dejan Kutle <kronikpillow@gmail.com>
2022-06-23 09:37:30 +0200

    added essentials script

---
+++ 800-essentials.sh
@@ -0,0 +1,24 @@
+sudo pacman -S --needed xorg-apps \
+xorg-fonts \
+xorg-drivers \
+pacutils \
+linux-tools \
+powertop \
+radeontop \
+iotop \
+schedtool \
+usbutils \
+lsof \
+strace \
+hdparm \
+#lha \
+#lzop \
+#p7zip \
+#unace \
+#zip \
+#github-cli \
+#expac \
+
+
+yay -S --needed inxi \
+downgrade
