6d73fecf9ff7db6bf79da8ec86e9986ae0ba0a9c
Dejan Kutle <kronikpillow@gmail.com>
2022-06-24 09:29:56 +0200

    fix grub settings

--- 250-grub.sh
+++ 250-grub.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-#pacman -S efibootmgr grub os-prober grub-btrfs intel-ucode
 
 echo "configuring grub settings"
-#sed -i "s/GRUB_DEFAULT=0/GRUB_DEFAULT=save/" /etc/default/grub
+sed -i "s/GRUB_DEFAULT=0/GRUB_DEFAULT=save/" /etc/default/grub
 sed -i '6 c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nowatchdog amdgpu.dc=0 zswap.enabled=0"' /etc/default/grub
-#sed -i "s/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/" /etc/default/grub
-#sed -i "s/#GRUB_DISABLE_SUBMENU=y/GRUB_DISABLE_SUBMENU=y/" /etc/default/grub
-#grep -q "GRUB_DISABLE_OS_PROBER=n" /etc/default/grub || sed -i "/GRUB_DISABLE_SUBMENU=y/a \GRUB_DISABLE_OS_PROBER=n" /etc/default/grub
+sed -i "s/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/" /etc/default/grub
+sed -i "s/#GRUB_DISABLE_SUBMENU=y/GRUB_DISABLE_SUBMENU=y/" /etc/default/grub
+sed -i "s/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=true" /etc/default/grub
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
