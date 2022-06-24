e0c594a6f4d01f4ec49b1335efd1be7ee7c37096
Dejan Kutle <kronikpillow@gmail.com>
2022-06-24 10:35:16 +0200

    added missing driver

--- 700-drivers.sh
+++ 700-drivers.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 sudo pacman -S --needed lib32-mesa \
-	mesa-utils \
-	lib32-mesa-utils \
 	xf86-video-amdgpu \
 	vulkan-radeon \
 	lib32-vulkan-radeon \
 	libva-mesa-driver \
 	lib32-libva-mesa-driver \
+	libva-utils \
 	mesa-vdpau \
 	lib32-mesa-vdpau \
+	libva-vdpau-driver \
+	lib32-libva-vdpau-driver \
+	vdpauinfo \
+	mesa-utils \
+	lib32-mesa-utils \
 	opencl-mesa \
-	lib32-opencl-mesa \
-	libva-utils \
-	vdpauinfo
+	lib32-opencl-mesa
