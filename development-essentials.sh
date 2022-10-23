a558695a4c2c987ecd626128eeb00ab5c2aa2741
Dejan Kutle <kronikpillow@gmail.com>
2022-10-23 14:19:38 +0200

    stuff

---
+++ development-essentials.sh
@@ -0,0 +1,1 @@
+p -S --needed luarocks composer php jdk-openjd
---
+++ yay.sh
@@ -0,0 +1,20 @@
+#!bin/sh
+yay -Sy lf-git \
+nordic-bluish-accent-theme \
+brave-bin \
+sc-im \
+abook \
+zsh-fast-syntax-highlighting-git \
+task-spooler \
+simple-mtpfs \
+htop-vim \
+mutt-wizard-git \
+tremc \
+opustags \
+ttf-mononoki \
+nerd-fonts-mononoki \
+papirus-folders-nordic \
+pfetch \
+xdg-ninja \
+qt5-styleplugins \
+qt6gtk2
