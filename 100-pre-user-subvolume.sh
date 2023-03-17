29d4d7367477b9aba9da9c8a5f425f8fb6784f7f
KronikPillow <kronikpillow@gmail.com>
2023-03-18 00:14:25 +0100

    update subvolume names and mnt names

--- 100-pre-user-subvolume.sh
+++ 100-pre-user-subvolume.sh
@@ -5,7 +5,7 @@ echo "Creating a BTRFS flat subvolume layout for Arch"
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
 btrfs su cr /mnt/@opt
-btrfs su cr /mnt/@admin
+btrfs su cr /mnt/@root
 btrfs su cr /mnt/@srv
 btrfs su cr /mnt/@local
 btrfs su cr /mnt/@pkg
@@ -16,19 +16,11 @@ btrfs su cr /mnt/@flatpak
 btrfs su cr /mnt/@libvirt
 btrfs su cr /mnt/@machines
 btrfs su cr /mnt/@portables
-btrfs su cr /mnt/@snapshots_arch
-btrfs su cr /mnt/@snapshots_home
+btrfs su cr /mnt/@.snapshots
+btrfs su cr /mnt/@home.snapshots
 
 # setup mount points
-mkdir -p /mnt/@/{.snapshots,boot,home,mnt/{btrfs,data,backup,usb,c,d},opt,root,srv,usr/local,var/{cache/pacman/pkg,lib/{docker,flatpak,libvirt,machines,portables},log,tmp}}
+mkdir -p /mnt/@/{.snapshots,boot,home,mnt/{system,data,usb,windows-c,windows-d},opt,root,srv,usr/local,var/{cache/pacman/pkg,lib/{docker,flatpak,libvirt,machines,portables},log,tmp}}
 mkdir -p /mnt/@home/.snapshots
-
-# nodatacow, wondering if this is really needed?
-
-# chattr +C /mnt/@docker
-# chattr +C /mnt/@flatpak
-# chattr +C /mnt/@libvirt
-# chattr +C /mnt/@machines
-# chattr +C /mnt/@portables
 
 printf "\e[1;32mDone! \e[0m"
