c0b504b4192d65abf398c572a728a9390b50c575
KronikPillow <kronikpillow@gmail.com>
2023-04-27 00:02:51 +0200

    update

--- 100-pre-user-subvolume.sh
+++ 100-pre-user-subvolume.sh
@@ -17,7 +17,7 @@ btrfs su cr /mnt/@/srv
 btrfs su cr /mnt/@/root
 btrfs su cr /mnt/@/opt
 btrfs su cr /mnt/@/home
-btrfs su cr /mnt/@/boot/grub
+btrfs su cr /mnt/@/boot
 btrfs su cr /mnt/@/.snapshots
 mkdir -p /mnt/@/.snapshots/1
 btrfs su cr /mnt/@/.snapshots/1/snapshot
@@ -32,5 +32,17 @@ echo "
 </snapshot>" >> /mnt/@/.snapshots/1/info.xml
 
 btrfs subvolume set-default $(btrfs subvolume list /mnt | grep "@/.snapshots/1/snapshot" | grep -oP '(?<=ID )[0-9]+') /mnt
+
+chattr +C /mnt/@/var/tmp
+chattr +C /mnt/@/var/spool
+chattr +C /mnt/@/var/log
+chattr +C /mnt/@/var/lib/portables
+chattr +C /mnt/@/var/lib/machines
+chattr +C /mnt/@/var/lib/libvirt
+chattr +C /mnt/@/var/lib/flatpak
+chattr +C /mnt/@/var/lib/docker
+chattr +C /mnt/@/var/lib/containers
+chattr +C /mnt/@/var/lib/containers
+chattr +C /mnt/@/var/cache/pacman/pkg
 
 printf "\e[1;32m Remember to go to edit /mnt/@/.snapshots/1/info.xml and move current date and time to the date row between <date></date> and remove the excess date file \e[0m"
