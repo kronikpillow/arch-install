ac3231b800bea29cd0a492ae70892cd7283cc733
KronikPillow <kronikpillow@gmail.com>
2023-04-26 23:11:58 +0200

    even more updates

--- 100-pre-user-subvolume.sh
+++ 100-pre-user-subvolume.sh
@@ -27,13 +27,10 @@ echo "
 <snapshot>
 	<type>single</type>
 	<num>1</num>
-	<date></date>
+	<date>$(which date)</date>
 	<description>First Root Filesystem Created at Installation</description>
 </snapshot>" >> /mnt/@/.snapshots/1/info.xml
-btrfs su cr /mnt/@home/.snapshots/1/snapshot
-
 
 btrfs subvolume set-default $(btrfs subvolume list /mnt | grep "@/.snapshots/1/snapshot" | grep -oP '(?<=ID )[0-9]+') /mnt
 
 printf "\e[1;32m Remember to go to edit /mnt/@/.snapshots/1/info.xml and move current date and time to the date row between <date></date> and remove the excess date file \e[0m"
-printf "\e[1;32m Remember to go to edit /mnt/@home/.snapshots/1/info.xml and move current date and time to the date row between <date></date> and remove the excess date file \e[0m"
