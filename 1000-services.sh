ad6e12c04e61876d1a09cf4ce47814a5b9486e74
KronikPillow <kronikpillow@gmail.com>
2024-05-29 01:28:37 +0200

    update

--- 1000-services.sh
+++ 1000-services.sh
@@ -1,38 +1,13 @@
 #!/bin/sh
-cp -r etc/polkit-1/* /etc/polkit-1/
-cp -r etc/sudoers.d/* /etc/sudoers.d/
-cp -r etc/sysctl.d/* /etc/sysctl.d/
-cp -r etc/systemd/* /etc/systemd/
-# chmod +x usr/local/bin/powertune
-# cp -r usr/* /usr/
 
 timedatectl set-timezone Europe/Belgrade
 timedatectl set-ntp true
 
-systemctl enable dbus-broker.service
-systemctl start dbus-broker.service
-
-systemctl enable rtkit-daemon.service
-systemctl start rtkit-daemon.service
-
-systemctl enable cronie.service
-systemctl start cronie.service
+systemctl enable fcron.service
+systemctl start fcron.service
 
 systemctl enable grub-btrfsd.service
 systemctl start grub-btrfsd.service
-
-# systemctl enable fstrim.timer
-# systemctl start fstrim.timer
-
-systemctl enable pkgfile-update.timer
-systemctl start pkgfile-update.timer
-
-systemctl enable reflector.timer
-systemctl start reflector.timer
-systemctl start reflector.service
-
-# systemctl enable powertune.service
-# systemctl start powertune.service
 
 systemctl enable avahi-daemon.service
 systemctl start avahi-daemon.service
--- 300-pacstrap.sh
+++ 300-pacstrap.sh
@@ -1,70 +1,101 @@
-#!/bin/sh
+#!/bin/bash
+set -e  # Exit on error
 
-echo "pacstrapping the base system"
-pacstrap /mnt base \
-base-devel \
-linux \
-linux-firmware \
-intel-ucode \
-mtools \
-dosfstools \
-exfatprogs \
-ntfs-3g \
-btrfs-progs \
-reiserfsprogs \
-efibootmgr \
-grub \
-os-prober \
-man-db \
-man-pages \
-texinfo \
-bash-completion \
-zsh \
-zsh-completions \
-python \
-python-pip \
-nodejs \
-npm \
-go \
-go-tools \
-rust \
-cargo \
-neovim \
-lf \
-git \
-rsync \
-wget \
-plocate \
-pkgfile \
-reflector \
-cronie \
-dbus-broker \
-polkit \
-pacman-contrib \
-pacutils \
-arch-install-scripts \
-pigz \
-pbzip2 \
-alsa-firmware \
-alsa-plugins \
-alsa-utils \
-wireplumber \
-pipewire-alsa \
-pipewire-jack \
-pipewire-pulse \
-gst-plugin-pipewire \
-rtkit \
-zram-generator \
-networkmanager \
-firewalld \
-xf86-video-amdgpu \
-grub-btrfs \
-inotify-tools \
-snapper \
+# Function to install packages
+pacstrap_base() {
+  echo "Pacstrapping the base system"
+
+packages=(
+base
+base-devel
+linux-zen
+linux-firmware
+intel-ucode
+mtools
+dosfstools
+exfatprogs
+ntfs-3g
+btrfs-progs
+reiserfsprogs
+efibootmgr
+grub
+os-prober
+man-db
+man-pages
+texinfo
+bash-completion
+zsh
+zsh-completions
+fish
+python
+nodejs
+npm
+go
+go-tools
+rust
+neovim
+lf
+git
+rsync
+wget
+plocate
+pkgfile
+fcron
+dbus-broker
+polkit
+pacman-contrib
+pacutils
+arch-install-scripts
+pigz
+pbzip2
+alsa-utils
+wireplumber
+pipewire-alsa
+pipewire-jack
+pipewire-pulse
+gst-plugin-pipewire
+rtkit
+zram-generator
+networkmanager
+firewalld
+grub-btrfs
+inotify-tools
+snapper
 snap-pac
+)
 
-sleep 5
+  pacman -Sy --needed "${packages[@]}"
 
-echo "generating fstab"
-genfstab -U /mnt >> /mnt/etc/fstab
-printf "\e[1;32mDone! \e[0m"
+  echo "Pacstrap complete."
+}
+
+# Function to modify grub files
+modify_grub_files() {
+  # List of files to modify
+  files=("/etc/grub.d/10_linux" "/etc/grub.d/20_linux_xen")
+
+  # Loop through each file
+  for file in "${files[@]}"; do
+    if [[ -f "$file" ]]; then
+      echo "Modifying $file"
+      sed -i 's/rootflags=subvol=${rootsubvol} //g' "$file"
+      echo "Modification complete for $file."
+    else
+      echo "File $file does not exist."
+    fi
+  done
+}
+
+# Function to generate fstab
+generate_fstab() {
+  echo "Generating fstab"
+  genfstab -U /mnt >> /mnt/etc/fstab
+  printf "\e[1;32mDone! \e[0m"
+}
+
+# Main script logic
+pacstrap_base
+modify_grub_files
+generate_fstab
+
+echo "Script completed."
