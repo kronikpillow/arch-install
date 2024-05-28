6397440d95730653ea89fc8c95e10b3addc4ff1f
KronikPillow <kronikpillow@gmail.com>
2024-05-29 00:31:39 +0200

    Update to correct SUSE Layout + User Subvolume

--- 100-pre-user-subvolume-root.sh
+++ 100-pre-user-subvolume-root.sh
@@ -1,29 +1,42 @@
 #!/bin/sh
+
+# Create a BTRFS flat subvolume layout for root
 echo "Creating a BTRFS flat subvolume layout for root"
-btrfs su cr /mnt/@ # this subvolume is not mounted in fstab, the subvolumes of this subvolume are mounted in to @/.snapshots/1/snapshot which will be the first root filesystem
-mkdir -p /mnt/@/{boot,usr,var/lib}
-btrfs su cr /mnt/@/var/tmp
-btrfs su cr /mnt/@/var/spool
-btrfs su cr /mnt/@/var/log
-btrfs su cr /mnt/@/var/lib/portables
-btrfs su cr /mnt/@/var/lib/machines
-btrfs su cr /mnt/@/var/lib/libvirt
-btrfs su cr /mnt/@/var/lib/flatpak
-# btrfs su cr /mnt/@/var/lib/docker
-btrfs su cr /mnt/@/var/lib/containers
-btrfs su cr /mnt/@/var/crash
-btrfs su cr /mnt/@/var/cache
+
+# Create the subvolume for the kronikpillow user
+btrfs su cr /mnt/@kronikpillow/
+
+# Create the subvolumes for the kronikpillow user's directories
+btrfs su cr /mnt/@kronikpillow/Downloads
+btrfs su cr /mnt/@kronikpillow/Music
+btrfs su cr /mnt/@kronikpillow/Pictures
+btrfs su cr /mnt/@kronikpillow/Videos
+btrfs su cr /mnt/@kronikpillow/.cache
+btrfs su cr /mnt/@kronikpillow/.local
+btrfs su cr /mnt/@kronikpillow/.steam
+btrfs su cr /mnt/@kronikpillow/.var
+btrfs su cr /mnt/@kronikpillow/.snapshots
+
+# Create the top-level subvolume for the installation system
+btrfs su cr /mnt/@
+
+# Create the necessary directories for the root subvolume
+mkdir -p /mnt/@/{boot,usr}
+
+# Create the subvolumes for the various directories
+btrfs su cr /mnt/@/var
 btrfs su cr /mnt/@/usr/local
 btrfs su cr /mnt/@/srv
 btrfs su cr /mnt/@/root
 btrfs su cr /mnt/@/opt
 btrfs su cr /mnt/@/boot/grub
 btrfs su cr /mnt/@/home/
-btrfs su cr /mnt/@/home/.snapshots
+
+# Create the snapshot directory and the initial snapshot
 btrfs su cr /mnt/@/.snapshots
-mkdir -p    /mnt/@/.snapshots/1
+mkdir -p /mnt/@/.snapshots/1
 btrfs su cr /mnt/@/.snapshots/1/snapshot
-touch       /mnt/@/.snapshots/1/info.xml
+touch /mnt/@/.snapshots/1/info.xml
 cat <<EOF > /mnt/@/.snapshots/1/info.xml
 <?xml version="1.0"?>
 <snapshot>
@@ -34,21 +47,17 @@ cat <<EOF > /mnt/@/.snapshots/1/info.xml
 </snapshot>
 EOF
 
-mkdir -p /mnt/@/.snapshots/1/snapshot/{.snapshots,boot/{efi,grub},home,mnt/{btrfs,data,usb},opt,root,srv,usr/local,var/{cache,crash,lib/{containers,docker,flatpak,libvirt,machines,portables},log,tmp}}
+# Create the necessary directories for the initial file system
+mkdir -p /mnt/@/.snapshots/1/snapshot/{.snapshots,boot,efi,home,mnt/{btrfs,data,windows-c,windows-d,usb},opt,root,srv,usr/local,var}
 
+# Set the initial snapshot as the default for the root subvolume
 btrfs subvolume set-default $(btrfs subvolume list /mnt | grep "@/.snapshots/1/snapshot" | grep -oP '(?<=ID )[0-9]+') /mnt
-btrfs quota enable /mnt
-btrfs subvolume list -t /mnt
-printf "\e[1;32m Btrfs subvolume layout with snapper rollback capabilities created. \e[0m"
 
-# btrfs su cr /mnt/@/home/kronikpillow/.cache
-# btrfs su cr /mnt/@/home/kronikpillow/.local/share/containers
-# btrfs su cr /mnt/@/home/kronikpillow/.local/share/flatpak
-# btrfs su cr /mnt/@/home/kronikpillow/.local/share/Steam
-# btrfs su cr /mnt/@/home/kronikpillow/.var
-# btrfs su cr /mnt/@/home/kronikpillow/Documents
-# btrfs su cr /mnt/@/home/kronikpillow/Downloads
-# btrfs su cr /mnt/@/home/kronikpillow/Music
-# btrfs su cr /mnt/@/home/kronikpillow/Pictures
-# btrfs su cr /mnt/@/home/kronikpillow/Videos
-# btrfs su cr /mnt/@/home/kronikpillow/repos
+# Enable BTRFS quota
+btrfs quota enable /mnt
+
+# Display the list of subvolumes
+btrfs subvolume list -t /mnt
+
+# Print a success message
+printf "\e[1;32m Btrfs subvolume layout with snapper rollback capabilities created. \e[0m"
