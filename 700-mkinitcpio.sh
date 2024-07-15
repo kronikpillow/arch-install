05e4b827fdede72bd279ad79cc4a07236e87ec4f
KronikPillow <kronikpillow@gmail.com>
2024-07-15 23:44:55 +0200

    updates

--- 700-mkinitcpio.sh
+++ 700-mkinitcpio.sh
@@ -11,11 +11,11 @@ for file in "${backup_files[@]}"; do
 done
 
 echo "Configuring mkinitcpio settings"
-#sed -i -e "1 s/.*/MODULES=(btrfs amdgpu)/" \
-#       -e "2 s/.*/BINARIES=(\/usr\/bin\/btrfs)/" \
-#       -e "3 s/.*/FILES=()/" \
-#       -e "4 s/.*/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block filesystems fsck grub-btrfs-overlayfs)" /etc/mkinitcpio.conf.d/mkinitcpio.conf
+sed -i -e "7 s/.*/MODULES=(btrfs amdgpu)/" \
+	-e "14 s/.*/BINARIES=(\/usr\/bin\/btrfs)/" \
+	-e "19 s/.*/FILES=()/" \
+	-e "55 s/.*/HOOKS=(base systemd autodetect microcode modconf kms keyboard sd-vconsole block filesystems fsck grub-btrfs-overlayfs)" /etc/mkinitcpio.conf.d/mkinitcpio.conf
 
-# mkinitcpio -P
+mkinitcpio -P
 
 printf "\e[1;32m%s\e[0m\n" "Done!"
